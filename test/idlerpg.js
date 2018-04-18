105aa8d36132ddaff58ae80b17fe27c2d4324259
Eric Martindale <eric@ericmartindale.com>
2018-04-18 03:42:39 +0200

    Improve test coverage

--- test/idlerpg.js
+++ test/idlerpg.js
@@ -7,4 +7,41 @@ describe('IdleRPG', function () {
   it('should expose a constructor', function () {
     assert(IdleRPG instanceof Function);
   });
+
+  it('should start without a configuration', function (done) {
+    let rpg = new IdleRPG();
+
+    rpg.on('ready', async function () {
+      rpg.stop().then(done);
+    });
+
+    rpg.start();
+  });
+
+  it('should correctly count 10 ticks', function (done) {
+    let rpg = new IdleRPG({ interval: 10 });
+    let count = 0;
+
+    rpg.on('tick', function () {
+      if (++count === 10) {
+        rpg.stop().then(done);
+      }
+    });
+
+    rpg.start();
+  });
+
+  it('should consume a series of events', function (done) {
+    let rpg = new IdleRPG({ interval: 10 });
+    let count = 0;
+
+    rpg.on('tick', function () {
+      if (++count === 10) {
+        rpg.stop().then(done);
+      }
+    });
+
+    rpg.start();
+  });
+
 });
